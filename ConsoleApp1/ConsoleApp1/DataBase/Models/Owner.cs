d1a70f104100c85561f77d8485958b1faf33c7b1
unknown <you@example.com>
2022-07-29 08:35:27 -0700

    Create Struktur

---
+++ ConsoleApp1/ConsoleApp1/DataBase/Models/Owner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.DataBase.Models
+{
+    class Owner : User
+    {
+        public Owner(string name, string lastName, string fIN) 
+            : base(name, lastName, fIN)
+        {
+
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/DataBase/Models/Seller.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.DataBase.Models
+{
+    class Seller : User
+    {
+        public string Education { get; set; }
+        public string Experience { get; set; }
+
+        public Seller(string name, string lastName, string fIN,string education,string experience)
+            : base(name, lastName, fIN)
+
+        {
+            Education = education;
+            Experience = experience;
+        }
+    }
+}
